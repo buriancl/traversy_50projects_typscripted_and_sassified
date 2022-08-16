8adac8cdf1f24711645eb40df7e04615966db773
Chris Burian <burian.christopher@gmail.com>
2022-08-16 10:53:56 -0400

    background-slider done

---
+++ 18-background-slider/script.ts
@@ -0,0 +1,40 @@
+const body = document.body as HTMLBodyElement
+const slides = document.querySelectorAll<HTMLDivElement>('.slide')
+const leftBtn = document.getElementById('left') as HTMLButtonElement
+const rightBtn = document.getElementById('right') as HTMLButtonElement
+
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+  activeSlide++
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0
+  }
+
+  setBgToBody()
+  setActiveSlide()
+})
+
+leftBtn.addEventListener('click', () => {
+  activeSlide--
+
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1
+  }
+
+  setBgToBody()
+  setActiveSlide()
+})
+
+setBgToBody()
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove('active'))
+
+  slides[activeSlide].classList.add('active')
+}
