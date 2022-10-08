7c8a6157481df1a1a64a4fcefd725763cf54645e
Chris Burian <burian.christopher@gmail.com>
2022-10-08 09:14:04 -0400

    completed custom range finder

---
+++ 44-custom-range-finder/script.ts
@@ -0,0 +1,39 @@
+const range = document.getElementById('range') as HTMLInputElement
+
+range?.addEventListener('input', (e) => {
+  const eventTarget = e.target as HTMLInputElement
+  if (eventTarget != null) {
+    const value = +eventTarget.value
+    const label = eventTarget.nextElementSibling as HTMLLabelElement
+
+    const rangeWidth = getComputedStyle(eventTarget).getPropertyValue('width')
+    if (label != null) {
+      const labelWidth = getComputedStyle(label).getPropertyValue('width')
+
+      const numWidth = +rangeWidth.substring(0, rangeWidth.length - 2)
+      const numLabelWidth = +labelWidth.substring(0, labelWidth.length - 2)
+      const max = +eventTarget.max
+      const min = +eventTarget.min
+      const left =
+        value * (numWidth / max) -
+        numLabelWidth / 2 +
+        scale(value, min, max, 10, -10)
+
+      label.style.left = `${left}px`
+
+      console.log(left)
+
+      label.innerHTML = value.toString()
+    }
+  }
+})
+
+const scale = (
+  num: number,
+  in_min: number,
+  in_max: number,
+  out_min: number,
+  out_max: number
+) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
