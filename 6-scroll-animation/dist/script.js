303a8cdceeb1d5eb22248bb4128213559d92e9e5
Chris Burian <burian.christopher@gmail.com>
2022-10-14 11:05:54 -0400

    generated boxes with TS in scroll animation and made SASS changes

--- 6-scroll-animation/dist/script.js
+++ 6-scroll-animation/dist/script.js
@@ -1,4 +1,14 @@
 "use strict";
+const boxAmount = 22;
+for (let i = 0; i < boxAmount; i++) {
+    generateBox();
+}
+function generateBox() {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.innerHTML = `<h2>Content</h2>`;
+    document.body.append(box);
+}
 const boxes = document.querySelectorAll('.box');
 window.addEventListener('scroll', checkBoxes);
 checkBoxes();
--- 6-scroll-animation/script.ts
+++ 6-scroll-animation/script.ts
@@ -1,3 +1,16 @@
+const boxAmount: number = 22
+
+for (let i = 0; i < boxAmount; i++) {
+  generateBox()
+}
+
+function generateBox() {
+  const box = document.createElement('div')
+  box.classList.add('box')
+  box.innerHTML = `<h2>Content</h2>`
+  document.body.append(box)
+}
+
 const boxes = document.querySelectorAll<HTMLDivElement>('.box')
 
 window.addEventListener('scroll', checkBoxes)
