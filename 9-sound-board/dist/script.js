e79b0e3b2f2fbf99649e59b900e9f2b18ff999ba
Chris Burian <burian.christopher@gmail.com>
2022-10-16 10:15:18 -0400

    more dynamic sound board and SASS changes

--- 9-sound-board/dist/script.js
+++ 9-sound-board/dist/script.js
@@ -1,19 +1,53 @@
 "use strict";
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
-sounds.forEach((sound) => {
+const soundBoard = [
+    {
+        soundTitle: 'applause',
+        soundSrc: 'sounds/sound-board_sounds_applause.mp3',
+    },
+    {
+        soundTitle: 'boo',
+        soundSrc: 'sounds/sound-board_sounds_boo.mp3',
+    },
+    {
+        soundTitle: 'gasp',
+        soundSrc: 'sounds/sound-board_sounds_gasp.mp3',
+    },
+    {
+        soundTitle: 'tada',
+        soundSrc: 'sounds/sound-board_sounds_tada.mp3',
+    },
+    {
+        soundTitle: 'victory',
+        soundSrc: 'sounds/sound-board_sounds_victory.mp3',
+    },
+    {
+        soundTitle: 'wrong',
+        soundSrc: 'sounds/sound-board_sounds_wrong.mp3',
+    },
+];
+soundBoard.forEach((sound) => {
+    const soundEl = document.createElement('audio');
+    soundEl.classList.add(`${sound.soundTitle}Sound`);
+    soundEl.setAttribute('src', sound.soundSrc);
+    document.body.prepend(soundEl);
     const btn = document.createElement('button');
     btn.classList.add('btn');
-    btn.innerText = sound;
+    btn.innerText = sound.soundTitle;
+    console.log(btn, soundEl);
     btn.addEventListener('click', () => {
         stopSongs();
-        document.getElementById(sound).play();
+        let songEl = document.querySelector(`.${sound.soundTitle}Sound`);
+        songEl === null || songEl === void 0 ? void 0 : songEl.play();
     });
-    document.getElementById('buttons').appendChild(btn);
+    let buttonsEL = document.getElementById('buttons');
+    buttonsEL.appendChild(btn);
 });
 function stopSongs() {
-    sounds.forEach((sound) => {
-        const song = document.getElementById(sound);
-        song.pause();
-        song.currentTime = 0;
+    soundBoard.forEach((sound) => {
+        const song = document.querySelector(`.${sound.soundTitle}Sound`);
+        if (song) {
+            song.pause();
+            song.currentTime = 0;
+        }
     });
 }
--- 9-sound-board/script.ts
+++ 9-sound-board/script.ts
@@ -1,31 +1,63 @@
-const sounds: string[] = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const soundBoard = [
+  {
+    soundTitle: 'applause',
+    soundSrc: 'sounds/sound-board_sounds_applause.mp3',
+  },
+  {
+    soundTitle: 'boo',
+    soundSrc: 'sounds/sound-board_sounds_boo.mp3',
+  },
+  {
+    soundTitle: 'gasp',
+    soundSrc: 'sounds/sound-board_sounds_gasp.mp3',
+  },
+  {
+    soundTitle: 'tada',
+    soundSrc: 'sounds/sound-board_sounds_tada.mp3',
+  },
+  {
+    soundTitle: 'victory',
+    soundSrc: 'sounds/sound-board_sounds_victory.mp3',
+  },
+  {
+    soundTitle: 'wrong',
+    soundSrc: 'sounds/sound-board_sounds_wrong.mp3',
+  },
+]
 
-sounds.forEach((sound) => {
+soundBoard.forEach((sound) => {
+  const soundEl = document.createElement('audio')
+  soundEl.classList.add(`${sound.soundTitle}Sound`)
+  soundEl.setAttribute('src', sound.soundSrc)
+  document.body.prepend(soundEl)
   const btn = document.createElement('button')
   btn.classList.add('btn')
 
-  btn.innerText = sound
+  btn.innerText = sound.soundTitle
 
+  console.log(btn, soundEl)
   btn.addEventListener('click', () => {
     stopSongs()
 
-    let songEl = document.getElementById(sound) as HTMLAudioElement
+    let songEl = document.querySelector<HTMLAudioElement>(
+      `.${sound.soundTitle}Sound`
+    )
 
-    songEl.play()
-
-    // document.getElementById(sound).play()
+    songEl?.play()
   })
 
   let buttonsEL = document.getElementById('buttons') as HTMLDivElement
-
   buttonsEL.appendChild(btn)
 })
 
 function stopSongs() {
-  sounds.forEach((sound) => {
-    const song = document.getElementById(sound) as HTMLAudioElement
-
-    song.pause()
-    song.currentTime = 0
+  soundBoard.forEach((sound) => {
+    const song = document.querySelector<HTMLAudioElement>(
+      `.${sound.soundTitle}Sound`
+    )
+    if (song) {
+      song.pause()
+      song.currentTime = 0
+    }
   })
 }
