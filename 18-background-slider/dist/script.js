9a2d681d56ec7be83b177f62f18d60f0ad28d4cb
Chris Burian <burian.christopher@gmail.com>
2022-11-16 11:50:03 -0500

    background slider images made add in TS

--- 18-background-slider/dist/script.js
+++ 18-background-slider/dist/script.js
@@ -1,8 +1,24 @@
 "use strict";
 const body = document.body;
-const slides = document.querySelectorAll('.slide');
+const slidercontainerEl = document.querySelector('.slider-container');
 const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
+const imageArr = [
+    'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+    'https://images.unsplash.com/photo-1511593358241-7eea1f3c84e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
+    'https://images.unsplash.com/photo-1495467033336-2effd8753d51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+    'https://images.unsplash.com/photo-1522735338363-cc7313be0ae0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80',
+    'https://images.unsplash.com/photo-1559087867-ce4c91325525?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+];
+imageArr.forEach((img) => generateSlide(img));
+function generateSlide(img) {
+    const slideEl = document.createElement('div');
+    slideEl.classList.add('slide');
+    slideEl.style.backgroundImage = `url(${img})`;
+    slidercontainerEl === null || slidercontainerEl === void 0 ? void 0 : slidercontainerEl.append(slideEl);
+}
+const slides = document.querySelectorAll('.slide');
+slides[0].classList.add('active');
 let activeSlide = 0;
 rightBtn.addEventListener('click', () => {
     activeSlide++;
--- 18-background-slider/script.ts
+++ 18-background-slider/script.ts
@@ -1,7 +1,29 @@
 const body = document.body as HTMLBodyElement
-const slides = document.querySelectorAll<HTMLDivElement>('.slide')
+const slidercontainerEl =
+  document.querySelector<HTMLDivElement>('.slider-container')
 const leftBtn = document.getElementById('left') as HTMLButtonElement
 const rightBtn = document.getElementById('right') as HTMLButtonElement
+
+const imageArr = [
+  'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+  'https://images.unsplash.com/photo-1511593358241-7eea1f3c84e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
+  'https://images.unsplash.com/photo-1495467033336-2effd8753d51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+  'https://images.unsplash.com/photo-1522735338363-cc7313be0ae0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80',
+  'https://images.unsplash.com/photo-1559087867-ce4c91325525?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+]
+
+imageArr.forEach((img) => generateSlide(img))
+
+function generateSlide(img: string) {
+  const slideEl = document.createElement('div')
+  slideEl.classList.add('slide')
+  slideEl.style.backgroundImage = `url(${img})`
+  slidercontainerEl?.append(slideEl)
+}
+
+const slides = document.querySelectorAll<HTMLDivElement>('.slide')
+
+slides[0].classList.add('active')
 
 let activeSlide = 0
 
