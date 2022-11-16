7d5106ad668517b6424a64359c6a6acb50d13c5b
Chris Burian <burian.christopher@gmail.com>
2022-11-16 10:35:27 -0500

    movie app modal and SASS updates

--- 17-movie-app/dist/script.js
+++ 17-movie-app/dist/script.js
@@ -28,6 +28,7 @@ const creditsDropdownEl = document.querySelector('.creditsDropdown');
 const directorEl = document.querySelector('.directorName');
 const castListEl = document.querySelector('.castList');
 const creditsToggleEl = document.querySelector('.creditsToggle');
+const releaseDateEl = document.querySelector('.date');
 //Get initial movies
 getMovies(API_URL);
 function getMovies(url) {
@@ -41,10 +42,18 @@ function showMovies(movies) {
     main.innerHTML = '';
     movies.forEach((movie) => {
         const { title, poster_path, vote_average, overview, id } = movie;
+        let verifiedPosterImg = '';
+        if (poster_path != null && poster_path != '') {
+            verifiedPosterImg = IMG_PATH + poster_path;
+        }
+        else {
+            verifiedPosterImg =
+                'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg';
+        }
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
-    <img src="${IMG_PATH + poster_path}" alt="${title}">
+    <img src="${verifiedPosterImg}" alt="${title}">
     <div class='movie-info'>
     <h3>${title}</h3>
     <span class="${getClassByRate(vote_average)}">${vote_average}</span>
@@ -65,21 +74,24 @@ function showMovies(movies) {
 //Get movie data
 function getMovieData(movieId) {
     return __awaiter(this, void 0, void 0, function* () {
-        const res = yield fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=b0a402eca41caa9931010586ca76e936&language=en-US&append_to_response=release_dates,credits`);
+        const res = yield fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=b0a402eca41caa9931010586ca76e936&language=en-US&append_to_response=release_dates,credits,recommendations,reviews`);
         const data = yield res.json();
         createModal(data);
     });
 }
 function createModal(movieData) {
-    console.log(movieData);
-    if (genreListEl) {
-        genreListEl.innerHTML = '';
-    }
-    if (castListEl) {
-        castListEl.innerHTML = '';
-    }
+    console.log(movieData.release_date);
+    clearElements();
     if (creditsDropdownEl) {
         creditsDropdownEl.classList.remove('active');
+    }
+    let verifiedPosterImg = '';
+    if (movieData.poster_path != null && movieData.poster_path != '') {
+        verifiedPosterImg = IMG_PATH + movieData.poster_path;
+    }
+    else {
+        verifiedPosterImg =
+            'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg';
     }
     movieData.genres.forEach((genre) => {
         const genreEl = document.createElement('li');
@@ -89,18 +101,53 @@ function createModal(movieData) {
     if (modalTitleEl && taglineEl && moviePosterEL && overViewEl && runtimeEl) {
         modalTitleEl.innerText = `${movieData.title}`;
         taglineEl.innerText = `${movieData.tagline}`;
-        moviePosterEL.src = `${IMG_PATH + movieData.poster_path}`;
+        moviePosterEL.src = `${verifiedPosterImg}`;
         moviePosterEL.alt = `${movieData.title}`;
         overViewEl.innerText = `${movieData.overview}`;
         runtimeEl.innerText = `${movieData.runtime}min`;
+    }
+    if (releaseDateEl) {
+        releaseDateEl.innerText = `${getReleaseDate()}`;
+    }
+    function getReleaseDate() {
+        const monthsArr = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ];
+        const splitDateArr = movieData.release_date.split('-');
+        console.log(splitDateArr);
+        const releaseYear = splitDateArr[0];
+        const releaseMonth = monthsArr[parseInt(splitDateArr[1])];
+        const releaseDay = splitDateArr[2];
+        console.log('releaseYear ===> ', releaseYear, 'releaseMonth ===> ', releaseMonth, 'releaseDay ===> ', releaseDay);
+        return `${releaseMonth} ${releaseDay}, ${releaseYear}`;
     }
     const releaseDates = movieData.release_dates.results;
     const usReleases = releaseDates.filter((releaseObj) => {
         return releaseObj.iso_3166_1 === 'US';
     });
-    const usCert = usReleases[0];
+    let usCert = [];
+    if (usReleases.length != 0) {
+        usCert = usReleases[0];
+    }
+    else {
+        usCert.push('NR');
+    }
     if (mpaaRatingEl) {
-        if (usCert.release_dates[0].certification !== '') {
+        if (usCert[0] === 'NR') {
+            mpaaRatingEl.innerText = 'NR';
+        }
+        else if (usCert.release_dates[0].certification !== '') {
             mpaaRatingEl.innerText = usCert.release_dates[0].certification;
         }
         else if (usCert.release_dates.length > 1 &&
@@ -116,37 +163,55 @@ function createModal(movieData) {
     }
     if (castListEl) {
         const castList = getCast(movieData.credits.cast);
-        castList.forEach((castMember) => {
-            let verifiedProfileImgPath = '';
-            if (castMember.profileImg != null) {
-                verifiedProfileImgPath = IMG_PATH + castMember.profileImg;
-            }
-            else {
-                verifiedProfileImgPath =
-                    'https://www.kindpng.com/picc/m/144-1447559_profile-icon-missing-profile-picture-icon-hd-png.png';
-            }
-            const actorEl = document.createElement('div');
-            actorEl.classList.add('actorInfo');
-            actorEl.innerHTML = `
+        if (castList.length === 0) {
+            const noActorsEl = document.createElement('p');
+            noActorsEl.innerText = 'No cast information found';
+            castListEl.append(noActorsEl);
+        }
+        else {
+            castList.forEach((castMember) => {
+                let verifiedProfileImgPath = '';
+                if (castMember.profileImg != null) {
+                    verifiedProfileImgPath = IMG_PATH + castMember.profileImg;
+                }
+                else {
+                    verifiedProfileImgPath =
+                        'https://www.kindpng.com/picc/m/144-1447559_profile-icon-missing-profile-picture-icon-hd-png.png';
+                }
+                const actorEl = document.createElement('div');
+                actorEl.classList.add('actorInfo');
+                actorEl.innerHTML = `
       <img class="profilePic" src='${verifiedProfileImgPath}' alt='${castMember.name}' />
       <div class="namesContainer">
       <p>${castMember.name}</p>
       <p>${castMember.character}</p>
       </div>
       `;
-            castListEl.append(actorEl);
-        });
+                castListEl.append(actorEl);
+            });
+        }
+    }
+}
+function clearElements() {
+    if (genreListEl && directorEl && castListEl) {
+        genreListEl.innerHTML = '';
+        directorEl.innerText = '';
+        castListEl.innerHTML = '';
     }
 }
 function getDirector(crew) {
     const director = crew.filter((crewMember) => {
         return crewMember.job === 'Director';
     });
-    return director[0].name;
+    if (director.length === 0) {
+        return 'Director information not found.';
+    }
+    else {
+        return director[0].name;
+    }
 }
 function getCast(cast) {
     const castList = [];
-    console.log(cast);
     if (cast.length >= 10) {
         for (let i = 0; i < 10; i++) {
             castList.push({
@@ -165,7 +230,6 @@ function getCast(cast) {
             });
         }
     }
-    console.log(castList);
     return castList;
 }
 creditsToggleEl === null || creditsToggleEl === void 0 ? void 0 : creditsToggleEl.addEventListener('click', () => {
